7cb86b983cac5e8746174e0c04231786236ea83d
Samuel Bolibruch <samo.bolibruch@gmail.com>
2024-11-19 13:53:46 +0100

    Created PetternLockView component using Pattern Lock library

--- app/src/main/java/com/example/passpoint/MainActivity.kt
+++ app/src/main/java/com/example/passpoint/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.example.passpoint
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,23 +22,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.behametrics.logger.Logger
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.passpoint.services.AuthManager
 import com.example.passpoint.ui.theme.PassPointTheme
-
 
 class MainActivity : ComponentActivity() {
     private val authManager = AuthManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Logger.start(this)
         setContent {
             PassPointTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -67,17 +61,19 @@ fun PatternInput(modifier: Modifier = Modifier, authManager: AuthManager) {
 
             Spacer(modifier = Modifier.weight(1f))
 
-            repeat(3) { rowIndex ->
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    repeat(3) { columnIndex ->
-                        CircularButton(index = rowIndex * 3 + columnIndex + 1)
-                    }
-                }
-            }
+            // Vloženie PatternLockView pomocou AndroidView
+            AndroidView(
+                modifier = Modifier.fillMaxWidth(),
+                factory = { context ->
+                    // Načítanie XML layoutu
+                    val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                    val view = inflater.inflate(R.layout.pattern_lock_view, null)
 
-            // Add a Spacer for spacing between pattern and logout button
+                    // Vrátenie root view (LinearLayout) z layoutu
+                    view
+                }
+            )
+
             Spacer(modifier = Modifier.height(32.dp))
 
             // Logout button at the bottom
@@ -106,24 +102,3 @@ fun PatternInput(modifier: Modifier = Modifier, authManager: AuthManager) {
         }
     }
 }
-
-@Composable
-fun CircularButton(index: Int) {
-    Button(
-        onClick = { /* Handle button click */ },
-        shape = CircleShape,
-        modifier = Modifier
-            .size(64.dp) // Set button size
-            .background(Color.LightGray, shape = CircleShape)
-    ) {
-        Text(text = index.toString(), color = Color.Black)
-    }
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun PatternInputPreview() {
-//    PassPointTheme {
-//        PatternInput()
-//    }
-//}
